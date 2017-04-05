64e39f8406a9f153d54eed64767f674c9f476757
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2017-04-05 17:27:00 +0100

    Deleted some unwanted resources.

--- extension.js
+++
@@ -1,7 +0,0 @@
-   
-function initialize() {
-  // Called on extension button press!
-  alert("press");
-}
-google.setOnLoadCallback(initialize);
-	
--- main.js
+++
@@ -1,8 +0,0 @@
-   
-function initialize() {
-  // Called on extension button press!
-  alert("press");
-}
-google.setOnLoadCallback(initialize);
-	
-
